0985e26bd390db1a30cd3bf8fbf116d40690e78a
d-hamasato <dafubangli@gmail.com>
2020-12-08 05:05:27 +0000

    [add] ユーザー編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :index, :show, :destroy]
 end

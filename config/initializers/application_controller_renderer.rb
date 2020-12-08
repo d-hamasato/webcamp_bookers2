24f1243b5a9e39d2df34bb2c60400033159e99ea
d-hamasato <dafubangli@gmail.com>
2020-12-08 05:43:35 +0000

    [add] サイドバー 新規投稿移設、プロフィール表示

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bb35e79fb8204a96d377f7faec7929fc1582f876432d79c4b095de043ec47918011044439b9840e4fae68a22a266fd80a5be4906f13d99e943a29162f6894cb2'
